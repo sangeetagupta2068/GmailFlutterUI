0092122de6d154bb0886f63c52338ff16f0c728d
sangeetagupta2068 <tinnigupta1998@gmail.com>
2018-11-01 03:54:14 +0530

    1 commit

--- lib/main.dart
+++ lib/main.dart
@@ -204,9 +204,10 @@ class MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ComposeEmailPageState()),
-        );},
+            context,
+            MaterialPageRoute(builder: (context) => ComposeEmailPageState()),
+          );
+        },
         child: Icon(Icons.edit),
         backgroundColor: Colors.red,
       ),
@@ -214,102 +215,100 @@ class MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class ComposeEmailPageState extends StatelessWidget{
+class ComposeEmailPageState extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        leading: GestureDetector(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          leading: GestureDetector(
             child: Padding(
-              padding: const EdgeInsets.only(left:4.0),
-              child: Icon(Icons.arrow_back,color: Colors.white,size:25.0),
+              padding: const EdgeInsets.only(left: 4.0),
+              child: Icon(Icons.arrow_back, color: Colors.white, size: 25.0),
             ),
-            onTap: (){ Navigator.of(context).pop(); },
-            ),
-        title: new Text(
-          "Compose",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 21.0,
-            fontWeight: FontWeight.w400,
+            onTap: () {
+              Navigator.of(context).pop();
+            },
           ),
+          title: new Text(
+            "Compose",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 21.0,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 13.0),
+              child: Icon(
+                Icons.attachment,
+                size: 25.0,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 13.0),
+              child: Icon(
+                Icons.send,
+                size: 23.0,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 13.0),
+              child: Icon(
+                Icons.more_vert,
+                size: 25.0,
+                color: Colors.white,
+              ),
+            ),
+          ],
         ),
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 13.0),
-            child: Icon(
-              Icons.attachment,
-              size: 25.0,
-              color: Colors.white,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right:13.0),
-            child: Icon(
-              Icons.send,
-              size: 23.0,
-              color: Colors.white,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right:13.0),
-            child: Icon(
-              Icons.more_vert,
-              size: 25.0,
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ),
-      body : Column(
-        children: <Widget>[
-          ListTile(
-              title:TextField(
+        body: Column(
+          children: <Widget>[
+            ListTile(
+              title: TextField(
                   decoration: InputDecoration(
-                    labelText: "From",
-                    border: InputBorder.none,
-                    labelStyle:TextStyle(color: Colors.black54,fontSize: 18.0),
-                  )),
-              trailing:Icon(Icons.keyboard_arrow_down),
-          ),
-          Divider(),
-          ListTile(
-             title:
-              TextField(
+                labelText: "From",
+                border: InputBorder.none,
+                labelStyle: TextStyle(color: Colors.black54, fontSize: 18.0),
+              )),
+              trailing: Icon(Icons.keyboard_arrow_down),
+            ),
+            Divider(),
+            ListTile(
+              title: TextField(
                   decoration: InputDecoration(
                       labelText: "To",
                       border: InputBorder.none,
-                      labelStyle:TextStyle(color: Colors.black54,fontSize: 18.0)
-                  )
-                  ),
-              trailing : Icon(Icons.keyboard_arrow_down),
-          ),
-          Divider(),
-          ListTile(
-            title: TextField
-              (decoration: InputDecoration(
-                hintText: "Subject",
-                border: InputBorder.none,
-                hintStyle:TextStyle(color: Colors.black54,fontSize: 18.0)
-                )
-               ),
-          ),
-          Divider(),
-          Expanded(
-            flex: 4,
-            child: ListTile(
+                      labelStyle:
+                          TextStyle(color: Colors.black54, fontSize: 18.0))),
+              trailing: Icon(Icons.keyboard_arrow_down),
+            ),
+            Divider(),
+            ListTile(
               title: TextField(
                   decoration: InputDecoration(
-                      hintText: "Compose Email",
+                      hintText: "Subject",
                       border: InputBorder.none,
-                      hintStyle: TextStyle(color: Colors.black54,fontSize: 18.0),
-                  )),
+                      hintStyle:
+                          TextStyle(color: Colors.black54, fontSize: 18.0))),
             ),
-          ),
-          Divider(),
-        ],
-      )
-    );
+            Divider(),
+            Expanded(
+              flex: 4,
+              child: ListTile(
+                title: TextField(
+                    decoration: InputDecoration(
+                  hintText: "Compose Email",
+                  border: InputBorder.none,
+                  hintStyle: TextStyle(color: Colors.black54, fontSize: 18.0),
+                )),
+              ),
+            ),
+            Divider(),
+          ],
+        ));
   }
 }
