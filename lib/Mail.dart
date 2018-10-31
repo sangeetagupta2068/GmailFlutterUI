9f381481d25ab371d97c89f76e9c1e83715b2072
sangeetagupta2068 <tinnigupta1998@gmail.com>
2018-11-01 03:56:13 +0530

    2 commit

--- lib/Mail.dart
+++ lib/Mail.dart
@@ -1,15 +1,12 @@
-
 class MailContent {
-
   String subject;
   String time;
   String sender;
   String message;
 
-  MailContent(this.subject,this.sender,this.time,this.message);
+  MailContent(this.subject, this.sender, this.time, this.message);
   String getSubject() => this.subject;
   String getSender() => this.sender;
   String getTime() => this.time;
   String getMessage() => this.message;
-
 }
